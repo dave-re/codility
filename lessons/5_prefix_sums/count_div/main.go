d80640d19af533f689dbc81191b260d7414a90fb
Dave <dave.choi@daumkakao.com>
2016-06-24 23:44:53 +0900

    Solving '5. Prefix Sums

---
+++ lessons/5_prefix_sums/count_div/main.go
@@ -0,0 +1,14 @@
+package main
+
+func Solution(A int, B int, K int) int {
+	if A == 0 && B == 0 {
+		return 1
+	}
+
+	a := ((A - 1) / K) + 1
+	if A == 0 {
+		a = 0
+	}
+	b := (B / K) + 1
+	return b - a
+}
---
+++ lessons/5_prefix_sums/genomic_range_query/main.go
@@ -0,0 +1,14 @@
+package main
+
+var (
+	cMap = map[string]int{
+		"A": 1,
+		"C": 2,
+		"G": 3,
+		"T": 4,
+	}
+)
+
+func Solution(S string, P []int, Q []int) []int {
+	return nil
+}
